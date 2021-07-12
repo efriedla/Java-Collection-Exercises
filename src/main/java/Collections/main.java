1cb88825533ce48514bee27cf679331c08010952
moocow <efriedla20852@gmail.com>
2021-07-11 21:53:35 -0400

    Initial commit

---
+++ src/main/java/Collections/main.java
@@ -0,0 +1,10 @@
+package Collections;
+
+/**
+ * Collections Q&A here: https://www.w3resource.com/java-exercises/collection/index.php
+ */
+public class main {
+	public static void main(String[] args){
+
+	}
+}
