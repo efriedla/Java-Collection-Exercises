76576383b2faf7aaa4dc65f4995f261dc4b2ab27
moocow <efriedla20852@gmail.com>
2021-07-12 09:25:59 -0400

    Questions added for rest of collections

---
+++ src/main/java/Collections/PriorityQueue/mainPriorityQueue.java
@@ -0,0 +1,31 @@
+package Collections.PriorityQueue;
+
+public class mainPriorityQueue {
+	public static void main(String[] args){
+		/**
+		 * 1. Write a Java program to create a new priority queue, add some colors (string) and print out the elements of the priority queue.
+		 *
+		 * 2. Write a Java program to iterate through all elements in priority queue.
+		 *
+		 * 3. Write a Java program to add all the elements of a priority queue to another priority queue.
+		 *
+		 * 4. Write a Java program to insert a given element into a priority queue.
+		 *
+		 * 5. Write a Java program to remove all the elements from a priority queue.
+		 *
+		 * 6. Write a Java program to count the number of elements in a priority queue.
+		 *
+		 * 7. Write a Java program to compare two priority queues.
+		 *
+		 * 8. Write a Java program to retrieve the first element of the priority queue.
+		 *
+		 * 9. Write a Java program to retrieve and remove the first element.
+		 *
+		 * 10. Write a Java program to convert a priority queue to an array containing all of the elements of the queue.
+		 *
+		 * 11. Write a Java program to convert a Priority Queue elements to a string representation.
+		 *
+		 * 12. Write a Java program to change priorityQueue to maximum priorityqueue.
+		 */
+	}
+}
